7aeebf0feb7b6b44d4490c36ba10c51a6e47800a
Daniela <daniela.paladinof@gmail.com>
2025-04-20 20:34:01 -0300

    feat: separate pipewire manager as "easy-pw" project

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,13 @@
 use log::LevelFilter;
-use pipewire_manager::PipeWireManager;
+use piper_rs::synth::PiperSpeechSynthesizer;
+use rodio::buffer::SamplesBuffer;
 use simplelog::{
     ColorChoice, CombinedLogger, Config, TermLogger, TerminalMode,
 };
+use std::path::Path;
 use std::thread;
 use std::time::Duration;
 
-mod pipewire_manager;
 fn main() {
     CombinedLogger::init(vec![TermLogger::new(
         LevelFilter::Debug,
@@ -16,30 +17,58 @@ fn main() {
     )])
     .unwrap();
     // Initialize PipeWire
-    let manager = PipeWireManager::default();
+    // let manager = PipeWireManager::default();
 
-    // Wait for 2 seconds
-    thread::sleep(Duration::from_secs(2));
+    // // Wait for 2 seconds
+    // thread::sleep(Duration::from_secs(2));
 
-    log::info!("Finished Loading!");
-    let mut objects = manager.objects.lock().unwrap();
+    // log::info!("Finished Loading!");
+    // let mut objects = manager.objects.lock().unwrap();
 
-    // objects.print_nodes();
+    // // objects.print_nodes();
 
-    let microphone = objects
-        .find_node_by_name("input.filter-chain-933-13")
-        .unwrap()
-        .id;
-    let source = objects.find_node_by_name("spotify").unwrap().id;
+    // let microphone = objects
+    //     .find_node_by_name("input.filter-chain-933-13")
+    //     .unwrap()
+    //     .id;
+    // let source = objects.find_node_by_name("spotify").unwrap().id;
 
-    std::mem::drop(objects);
-    log::info!("Found nodes: {microphone} and {source}");
+    // std::mem::drop(objects);
+    // log::info!("Found nodes: {microphone} and {source}");
 
-    manager.link_nodes(source, microphone);
-    manager.unlink_nodes(source, microphone);
+    // manager.link_nodes(source, microphone);
+    // manager.unlink_nodes(source, microphone);
 
 
-    log::info!("Event Sent");
+    // log::info!("Event Sent");
 
-    manager._main_thread.join().unwrap();
+    // manager._main_thread.join().unwrap();
+
+    let config_path = std::env::args().nth(1).expect("Please specify config path");
+    let text = "TEST".to_string();
+    let sid = std::env::args().nth(2);
+
+    let model = piper_rs::from_config_path(Path::new(&config_path)).unwrap();
+    // Set speaker ID
+    if let Some(sid) = sid {
+        let sid = sid.parse::<i64>().expect("Speaker ID should be number!");
+        model.set_speaker(sid);
+    }
+    let synth = PiperSpeechSynthesizer::new(model).unwrap();
+    let mut samples: Vec<f32> = Vec::new();
+    let audio = synth.synthesize_parallel(text, None).unwrap();
+    for result in audio {
+        samples.append(&mut result.unwrap().into_vec());
+    }
+
+    let (_stream, handle) = rodio::OutputStream::try_default().unwrap();
+    let sink = rodio::Sink::try_new(&handle).unwrap();
+
+    let buf = SamplesBuffer::new(1, 22050, samples);
+    sink.append(buf);
+
+    sink.sleep_until_end();
+
+
+
 }
